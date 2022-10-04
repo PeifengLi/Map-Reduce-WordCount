2d3479fc930f6980ee58f9b1498292a9de8e1778
Peifeng <102097656+PeifengLi@users.noreply.github.com>
2022-10-04 13:29:49 -0400

    Create localhost.sh

---
+++ Experiment 2/localhost.sh
@@ -0,0 +1,10 @@
+hadoop/hadoop-3.2.1/bin/mapred streaming 
+-D stream.num.map.output.key.fields=2 
+-D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator 
+-D mapred.text.key.comparator.options=-k2,2nr 
+-input /user/peifeng/output 
+-output /user/peifeng/output2 
+-mapper cat  
+-reducer cat 
+
+hadoop/hadoop-3.2.1/bin/hdfs dfs -get /user/peifeng/output/part-00000
